de141e4c3f4a1c46e97708870a9fba22a1554dbc
safa albukhari <bukharisafam@gmail.com>
2025-10-20 14:28:31 +0300

    My update  website

--- app.py
+++ app.py
@@ -5,23 +5,8 @@ import pandas as pd
 '''
 # TaxiFareModel front
 '''
-st.markdown('''
-Remember that there are several ways to output content into your web page...
 
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
 
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
 st.header("Select Ride Details")
 
 date = st.date_input("Pickup Date", datetime.today())
@@ -33,13 +18,7 @@ pickup_latitude = st.number_input("Pickup Latitude", value=40.748817)
 dropoff_longitude = st.number_input("Dropoff Longitude", value=-73.985428)
 dropoff_latitude = st.number_input("Dropoff Latitude", value=40.748817)
 passenger_count = st.number_input("Passenger Count", min_value=1, max_value=10, value=1)
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
 
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
 
 url = 'https://taxifare-803101541517.europe-west1.run.app'
 
@@ -47,16 +26,7 @@ if url == 'https://taxifare.lewagon.ai/predict':
 
     st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
 
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
 ## Finally, we can display the prediction to the user
-'''
 params = {
     "pickup_datetime": pickup_datetime,
     "pickup_longitude": pickup_longitude,
